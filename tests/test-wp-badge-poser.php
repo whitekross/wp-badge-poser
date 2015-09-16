4eba96d99bf8179972812747874270fc6ee373bd
Alessandro Fiore <fiore.alessandro@yahoo.it>
2015-09-16 14:14:32 +0200

    refactoring tests

--- tests/test-wp-badge-poser.php
+++ tests/test-wp-badge-poser.php
@@ -12,50 +12,60 @@ class WP_Badge_Poser_Test extends WP_UnitTestCase
     public function test_shortcodes_badge_poser() 
     {
 
+        // stable
         $content = '[badge-poser package="pugx/badge-poser"]';
         $shortcode_content = do_shortcode( $content );
         $this->assertNotEquals( $content, $shortcode_content );
-        // HTML Matcher
-        $matcher = [
-          'tag'  => 'img',
-          'attributes'  => 
-                [
-                'src' => 'https://poser.pugx.org/pugx/badge-poser/v/stable',
-                'alt' => 'Latest stable version of pugx/badge-poser'
-                ],
-          'parent'      => ['tag' => 'a', 'attributes' => ['href' => 'https://packagist.org/packages/pugx/badge-poser']]
-        ];
-        $this->assertTag($matcher, $shortcode_content);
 
+        $dom = $this->getDom($shortcode_content);
+
+        $atag = $dom->query('//a')->item(0);
+        $this->assertEquals( $atag->getAttribute('href'), 'https://packagist.org/packages/pugx/badge-poser' );
+
+        $imgtag = $dom->query('//img')->item(0);
+        $this->assertEquals( $imgtag->getAttribute('src'), 'https://poser.pugx.org/pugx/badge-poser/v/stable' );
+
+        // unstable
         $content = '[badge-poser package="pugx/badge-poser" version="unstable"]';
         $shortcode_content = do_shortcode( $content );
         $this->assertNotEquals( $content, $shortcode_content );
-        // HTML Matcher
-        $matcher = [
-          'tag'  => 'img',
-          'attributes'  => 
-                [
-                'src' => 'https://poser.pugx.org/pugx/badge-poser/v/unstable',
-                'alt' => 'Latest unstable version of pugx/badge-poser'
-                ],
-          'parent'      => ['tag' => 'a', 'attributes' => ['href' => 'https://packagist.org/packages/pugx/badge-poser']]
-        ];
-        $this->assertTag($matcher, $shortcode_content);
 
+        $dom = $this->getDom($shortcode_content);
+
+        $atag = $dom->query('//a')->item(0);
+        $this->assertEquals( $atag->getAttribute('href'), 'https://packagist.org/packages/pugx/badge-poser' );
+
+        $imgtag = $dom->query('//img')->item(0);
+        $this->assertEquals( $imgtag->getAttribute('src'), 'https://poser.pugx.org/pugx/badge-poser/v/unstable' );
+
+        // download
         $content = '[badge-poser package="pugx/badge-poser" download="total"]';
         $shortcode_content = do_shortcode( $content );
         $this->assertNotEquals( $content, $shortcode_content );
-        // HTML Matcher
-        $matcher = [
-          'tag'  => 'img',
-          'attributes'  => 
-                [
-                'src' => 'https://poser.pugx.org/pugx/badge-poser/d/total',
-                'alt' => 'total downloads for pugx/badge-poser'
-                ],
-          'parent'      => ['tag' => 'a', 'attributes' => ['href' => 'https://packagist.org/packages/pugx/badge-poser']]
-        ];
-        $this->assertTag($matcher, $shortcode_content);
+
+        $dom = $this->getDom($shortcode_content);
+
+        $atag = $dom->query('//a')->item(0);
+        $this->assertEquals( $atag->getAttribute('href'), 'https://packagist.org/packages/pugx/badge-poser' );
+
+        $imgtag = $dom->query('//img')->item(0);
+        $this->assertEquals( $imgtag->getAttribute('src'), 'https://poser.pugx.org/pugx/badge-poser/d/total' );
         
     }
+
+    private function getDom($content)
+    {
+        $dom = new \DOMDocument();  
+        //load the html  
+        $dom->loadHTML($content);
+        $dom->preserveWhiteSpace = false;
+        $xpath = new \DOMXPath($dom);
+        if(!empty($xpath)) {
+            return $xpath;
+        }
+
+        return false;
+        
+    }
+
 }
